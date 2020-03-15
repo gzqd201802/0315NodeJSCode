d62d398b776eca5f115d2faabfec590951c929cc
sujiehao <sujiehao@itcast.cn>
2020-03-15 16:47:56 +0800

    图片处理，css抽离

--- 02-课堂代码/webpack-demo/src/index.js
+++ 02-课堂代码/webpack-demo/src/index.js
@@ -4,8 +4,23 @@
 // 在当前文件中导入 css 样式文件
 import './style.css';
 
+// 导入 less 文件
+import './style.less';
+
+// 导入 logo.jpg 图片并保存到变量名 logo 中
+import logo from '../images/logo.jpg';
+
 // 写入到html的内容
 var element = document.createElement("div");
-element.innerHTML =  `<div class="red">hello webpack</div>`;
+// 有三个 div 结构
+//  1. 一个 div 上有 red 类名
+//  2. 另一个 div 上有 blue 类名
+//  3. 有一个图片标签，src 挖坑并填入 logo  
+element.innerHTML =  `
+    <div class="red">hello webpack</div>
+    <div class="blue">hello webpack and less</div>
+    <img src="${logo}" />
+`;
 
+// 把 div 写入到 body 中
 document.body.appendChild(element);
--- 02-课堂代码/webpack-demo/webpack.config.js
+++ 02-课堂代码/webpack-demo/webpack.config.js
@@ -1,6 +1,9 @@
 // 按需导入 path 模块
 const path = require('path');
 
+//  按需导入提取样式的webpack插件
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 // 导出打包配置
 module.exports = {
     // 打包哪个文件
@@ -17,15 +20,66 @@ module.exports = {
     module: {
         // 处理规则
         rules: [
+            // 处理 css 文件的规则配置
+            // {
+            //     // test 匹配 .css 结尾的文件
+            //     test: /\.css$/,			// 匹配css扩展名文件
+            //     // use 配置用什么加载器 处理 .css 结尾的文件
+            //     use: [					// 配置loader加载器
+            //         'style-loader',		// 2. 把css代码写入到网页中
+            //         'css-loader'		// 1. 读取css的代码
+            //     ]
+            // },
+            // 新添加一个处理 less 文件的规则配置
+            // {
+            //     test: /\.less$/,		// 匹配less扩展名文件
+            //     use:[				
+            //         'style-loader',		// 3. 把读取到的 css 代码写入到网页中
+            //         'css-loader',		// 2. 读取 css 的代码
+            //         'less-loader'		// 1. 解释编译 less 代码处理成 css 代码
+            //     ]	
+            // },
+
+            // 换成插件的两个新规则
+            // 用插件处理 .css 结尾的文件 
             {
-                // test 匹配 .css 结尾的文件
-                test: /\.css$/,			// 匹配css扩展名文件
-                // use 配置用什么加载器 处理 .css 结尾的文件
-                use: [					// 配置loader加载器
-                    'style-loader',		// 2. 把css代码写入到网页中
-                    'css-loader'		// 1. 读取css的代码
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({	// 提取css
+                    fallback: "style-loader",
+                    use: ["css-loader"]
+                })
+            },
+            // 用插件处理 .less 结尾的文件
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({	// 提取less
+                    fallback: "style-loader",
+                    use: ["css-loader", "less-loader"]
+                })
+            },
+            // 配置处理文件的 loader
+            {
+                // 主要处理 png|svg|jpg|gif 结尾的文件
+                test: /\.(png|svg|jpg|gif)$/,	// 匹配图片文件
+                use: [
+                    {
+                        // 配置 loader
+                        loader: "file-loader",              // 处理图片文件返回链接
+                        // 配置选项
+                        options: {
+                            publicPath: "./images/",   		//  引入图片时会在路径前面加上该选项
+                            // 输出到哪里
+                            outputPath: "images"            //  输出到dist下的images目录
+                        }
+                    }
                 ]
             }
         ]
-    }
+    },
+
+    // 需要启动的插件列表
+    plugins: [
+        // 启动插件
+        new ExtractTextPlugin('style/style.css') // 提取到dist的style文件夹中
+    ]
 };
